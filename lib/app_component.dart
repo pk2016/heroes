ba5bf9057c28bd447f9f9d4e82bacaebf97ba257
thomas <812588257@qq.com>
2018-12-28 23:45:34 +0800

    添加服务

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,17 +2,29 @@ import 'package:angular/angular.dart';
 
 import 'src/hero.dart';
 import 'src/hero_component.dart';
-import 'src/mock_heroes.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [coreDirectives,HeroComponent],
+  directives: [coreDirectives, HeroComponent],
   styleUrls: ['app_component.css'],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Tour of Heroes';
-  List<Hero> heroes = mockHeroes;
+  final HeroService _heroService;
+  List<Hero> heroes;
   Hero selected;
-  void onSelect(Hero hero) =>selected = hero;
+
+  AppComponent(this._heroService);
+
+  void onSelect(Hero hero) => selected = hero;
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
+
+  @override
+  void ngOnInit() => _getHeroes();
 }
